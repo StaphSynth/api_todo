78c22d5d72ec44839daf0509337e31a18febed71
David Allen <dallen@zendesk.com>
2018-01-28 15:23:48 +1100

    Add tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180128041855) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.boolean "complete"
+    t.string "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
